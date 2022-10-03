9c3c890a2bcfac4ac196af8de79296ec7ee429c7
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-10-03 16:50:13 +0300

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <Router>
+          <Navbar/>
+          <Switch>
+            <Route path="/" exact component = {Home} ></Route>
+            <Route path="/about" exact component = {About} ></Route>
+            <Route path="/services" exact component = {Services} ></Route>
+            <Route path="/sign-up" exact component = {SignUp} ></Route>
+          </Switch>
+          <Footer />
+        </Router>
+    </>
+  )
 }
 
 export default App;
