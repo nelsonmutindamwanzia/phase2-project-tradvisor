ca93d0fb26bbaf5d83ca39b76a70709c1d14c23f
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-10-04 14:33:17 +0300

    finalise on form styling and classNames

--- src/components/pages/AddDestination.js
+++ src/components/pages/AddDestination.js
@@ -32,16 +32,16 @@ function AddDestination() {
             <div className = "message-input">
                 <h4>Kindly fill in the form below to add a new Destination:</h4>
                 <form id="messageForm" onSubmit={handleSubmit}>
-                    <div className="form-group">
-                        <label  className="form-label">Image Source</label>
+                    <div className="mb-3">
+                        <label  className="form-label">Image Source:</label>
                         <input onChange={handleChange} value={newData.src} type="text" className="form-control" id="src" aria-describedby="src" placeholder='Enter the image address...'/>
                     </div>
-                    <div className="form-group">
-                        <label className="form-label">Text</label>
+                    <div className="mb-3">
+                        <label className="form-label">Destination Description:</label>
                         <textarea onChange={handleChange} value={newData.text} className="form-control" id="text" rows="3" placeholder='Enter the destination description...'></textarea>
                     </div>
-                    <div className="form-group">
-                        <label className="form-label">Label</label>
+                    <div className="mb-3">
+                        <label className="form-label">Label:</label>
                         <input onChange={handleChange} value={newData.label} type="text" min="0" className="form-control" id="label" aria-describedby="label" placeholder='Enter the destination name...'/>
                     </div>
                     
